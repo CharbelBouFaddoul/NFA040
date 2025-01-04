79c8e0ff14635161bf4acbd5dd2ddbaaf180cecc
CharbelBouFaddoul <94682316+CharbelBouFaddoul@users.noreply.github.com>
2025-01-04 18:35:44 +0200

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,6 +1,4 @@
 const expression = document.querySelector(".calcOutput");
-// used for resetting expression after result when clicking 'DEL' button
-let resetOutput = false;
 
 function Press(a){
     const operators = ["+", "-", "x", "/"];
@@ -12,14 +10,10 @@ function Press(a){
 
     if (currentContent.length < 17) {
         expression.innerHTML += a;
-        resetOutput = false;
     }
 }
 
 function Del(){
-    if (resetOutput){
-        expression.innerHTML = "";
-    }
     expression.innerHTML = expression.textContent.trim().slice(0,-1);
 }
 
@@ -37,7 +31,6 @@ function equal(){
         }
 
         expression.innerHTML = result;
-        resetOutput = true;
     } catch (error){
         alert('Enter a valid expression!');
         Reset();
